bc6e2e48acb7c9754126927141ddfff737130f02
kghimire7 <keshav.ghimire@tbdssab.ca>
2026-04-13 15:48:44 -0400

    Update sleep.ps1

--- sleep.ps1
+++ sleep.ps1
@@ -1,1 +1,5 @@
-powercfg /change monitor-timeout-ac 15 & powercfg /change standby-timeout-ac 0
+# Set display timeout to 15 minutes (AC)
+powercfg /change monitor-timeout-ac 15
+
+# Disable sleep (AC)
+powercfg /change standby-timeout-ac 0
